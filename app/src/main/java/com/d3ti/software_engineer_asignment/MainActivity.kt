44fa1f7a44327619df7564c38edf4a2f49041a8a
Raditya Firman Syaputra <radit@student.uns.ac.id>
2022-11-03 16:51:13 +0700

    Init Project

---
+++ app/src/main/java/com/d3ti/software_engineer_asignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.d3ti.software_engineer_asignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
